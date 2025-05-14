d77a712d38eb0063fa0c8aaea8740bbc28880be6
Nicola Bonelli <nicola.bonelli@huawei-partners.com>
2025-05-14 06:58:50 +0200

    Benchmark updated.
    Signed-off-by: Nicola Bonelli <nicola.bonelli@huawei-partners.com>

--- orion-format/benches/benchmark.rs
+++ orion-format/benches/benchmark.rs
@@ -30,7 +30,7 @@ where
 }
 
 #[inline(always)]
-fn header_lookup<'a>(name: &'a str, m: &'a HeaderMap, default_header: &'a HeaderValue) -> &'a HeaderValue {        
+fn header_lookup<'a>(name: &'a str, m: &'a HeaderMap, default_header: &'a HeaderValue) -> &'a HeaderValue {
     m.get(name).unwrap_or(default_header)
 }
 
@@ -42,33 +42,34 @@ fn benchmark_rust_format(c: &mut Criterion) {
         .unwrap();
 
     let response = Response::builder().status(StatusCode::OK).body(()).unwrap();
-    let start = DownStreamContext { start_time: std::time::SystemTime::now() };
-    let end = UpStreamContext { duration: Duration::from_millis(100), bytes_received: 128, bytes_sent: 256 };
-    //let mut sink = std::io::sink();
+    let start = InitContext { start_time: std::time::SystemTime::now() };
+    let end = FinishContext { duration: Duration::from_millis(100), bytes_received: 128, bytes_sent: 256 };
+    let mut sink = std::io::sink();
 
-    c.bench_function("Rust format formatter", |b| {
+    c.bench_function("Rust format!", |b| {
         b.iter(|| {
-            let s = black_box( eval_rust_format(
-                &DownStreamRequest(&request),
-                &DownStreamResponse(&response),
-                &start,
-                &end,                
-            ));
-            let _ = black_box(write_to_format(&mut sink, s.as_bytes()));
+            let s =
+                black_box(eval_rust_format(&DownstreamRequest(&request), &DownstreamResponse(&response), &start, &end));
+
+            _ = black_box(write_to_format(&mut sink, s.as_bytes()));
         })
     });
 }
 
 fn write_to_format<W: std::io::Write>(w: &mut W, log: &[u8]) -> std::io::Result<usize> {
-    let len= w.write(log)?;
+    let len = w.write(log)?;
     Ok(len)
-
 }
 
-fn eval_rust_format( request: &DownStreamRequest<()>, response: &DownStreamResponse<()>, downstream_context: &DownStreamContext, upstream_context: &UpStreamContext )->String{
+fn eval_rust_format(
+    request: &DownstreamRequest<()>,
+    response: &DownstreamResponse<()>,
+    init_context: &InitContext,
+    finish_context: &FinishContext,
+) -> String {
     let request = request.0;
     let response = response.0;
-    let start_time = &downstream_context.start_time;
+    let start_time = &init_context.start_time;
     let datetime_utc: DateTime<Utc> = start_time.clone().into();
     let rfc3339 = datetime_utc.to_rfc3339_opts(SecondsFormat::Millis, true);
 
@@ -85,12 +86,13 @@ fn eval_rust_format( request: &DownStreamRequest<()>, response: &DownStreamRespo
     };
 
     let response_code = response.status();
-    
-    let default_header:HeaderValue = HeaderValue::from_static("");
-    let x_envoy_upstream_service_time = header_lookup("X-ENVOY-UPSTREAM-SERVICE-TIME", request.headers(),&default_header);
-    let x_forwarded_for = header_lookup("SX-FORWARDED-FORER-AGENT", request.headers(),&default_header);
-    let user_agent = header_lookup("USER-AGENT", request.headers(),&default_header);
-    let x_request_id = header_lookup("X-REQUEST-ID", request.headers(),&default_header);
+
+    let default_header: HeaderValue = HeaderValue::from_static("");
+    let x_envoy_upstream_service_time =
+        header_lookup("X-ENVOY-UPSTREAM-SERVICE-TIME", request.headers(), &default_header);
+    let x_forwarded_for = header_lookup("SX-FORWARDED-FORER-AGENT", request.headers(), &default_header);
+    let user_agent = header_lookup("USER-AGENT", request.headers(), &default_header);
+    let x_request_id = header_lookup("X-REQUEST-ID", request.headers(), &default_header);
 
     format!(
         r#"[{}] "{} {} {} {} %RESPONSE_FLAGS% {} {} {} {} {} {} {} {} "%UPSTREAM_HOST%""#,
@@ -99,72 +101,18 @@ fn eval_rust_format( request: &DownStreamRequest<()>, response: &DownStreamRespo
         uri.path(),
         ver,
         response_code.as_u16(),
-        upstream_context.bytes_received,
-        upstream_context.bytes_sent,
-        upstream_context.duration.as_millis(),
+        finish_context.bytes_received,
+        finish_context.bytes_sent,
+        finish_context.duration.as_millis(),
         x_envoy_upstream_service_time.to_str().unwrap(),
         x_forwarded_for.to_str().unwrap(),
         user_agent.to_str().unwrap(),
         x_request_id.to_str().unwrap(),
         uri.authority().unwrap().host()
     )
-
 }
 
-
-
-fn benchmark_request(c: &mut Criterion) {
-    let request = Request::builder()
-        .uri("https://www.rust-lang.org/hello")
-        .header("User-Agent", "my-awesome-agent/1.0")
-        .body(())
-        .unwrap();
-
-    let response = Response::builder().status(StatusCode::OK).body(()).unwrap();
-    let start = DownStreamContext { start_time: std::time::SystemTime::now() };
-    let end = UpStreamContext { duration: Duration::from_millis(100), bytes_received: 128, bytes_sent: 256 };
-
-    let fmt = LogFormatter::try_new("%REQ(:PATH)%").unwrap();
-    c.bench_function("REQ(:PATH)", |b| {
-        b.iter(|| {
-            black_box(eval_format(
-                &DownStreamRequest(&request),
-                &DownStreamResponse(&response),
-                &start,
-                &end,
-                fmt.clone(),
-            ))
-        })
-    });
-
-    let fmt = LogFormatter::try_new("%REQ(:METHOD)%").unwrap();
-    c.bench_function("REQ(:METHOD)", |b| {
-        b.iter(|| {
-            black_box(eval_format(
-                &DownStreamRequest(&request),
-                &DownStreamResponse(&response),
-                &start,
-                &end,
-                fmt.clone(),
-            ))
-        })
-    });
-
-    let fmt = LogFormatter::try_new("%REQ(USER-AGENT)%").unwrap();
-    c.bench_function("REQ(USER-AGENT)", |b| {
-        b.iter(|| {
-            black_box(eval_format(
-                &DownStreamRequest(&request),
-                &DownStreamResponse(&response),
-                &start,
-                &end,
-                fmt.clone(),
-            ))
-        })
-    });
-}
-
-fn benchmark_default_format(c: &mut Criterion) {
+fn benchmark_log_formatter(c: &mut Criterion) {
     #[cfg(feature = "dhat-heap")]
     let _profiler = dhat::Profiler::new_heap();
 
@@ -179,21 +127,13 @@ fn benchmark_default_format(c: &mut Criterion) {
     let end = FinishContext { duration: Duration::from_millis(100), bytes_received: 128, bytes_sent: 256 };
 
     let fmt = LogFormatter::try_new(DEF_FMT).unwrap();
-    
-    c.bench_function("Envoy default formatter", |b| {
+    let mut sink = std::io::sink();
+
+    c.bench_function("LogFormatter: DownstreamRequest", |b| {
         b.iter(|| {
-            black_box(eval_format(
-                &DownStreamRequest(&request),
-                &DownStreamResponse(&response),
-                &start,
-                &end,
-                fmt.clone(),
-            ));
+            black_box(fmt.clone().with_context(&DownstreamRequest(&request)));
         })
     });
-
-    
-}
 
     c.bench_function("LogFormatter: DownstreamResponse", |b| {
         b.iter(|| {
@@ -207,9 +147,13 @@ fn benchmark_default_format(c: &mut Criterion) {
         })
     });
 
-    let fmt = LogFormatter::try_new(DEF_FMT).unwrap();
-    let mut sink = std::io::sink();
-    c.bench_function("Default formatter and write", |b| {
+    c.bench_function("LogFormatter: FinishContext", |b| {
+        b.iter(|| {
+            black_box(fmt.clone().with_context(&end));
+        })
+    });
+
+    c.bench_function("LogFromatter (full)", |b| {
         b.iter(|| {
             black_box(eval_format(
                 &DownstreamRequest(&request),
@@ -217,7 +161,19 @@ fn benchmark_default_format(c: &mut Criterion) {
                 &start,
                 &end,
                 fmt.clone(),
-            ));            
+            ));
+        })
+    });
+
+    c.bench_function("LogFormatter (full) + write", |b| {
+        b.iter(|| {
+            black_box(eval_format(
+                &DownstreamRequest(&request),
+                &DownstreamResponse(&response),
+                &start,
+                &end,
+                fmt.clone(),
+            ));
             _ = black_box(|| fmt.write_to(&mut sink));
         })
     });
