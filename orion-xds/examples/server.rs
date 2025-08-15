793e876e5350ff47141f65fc3cbb9dee42ca4d9a
YaoZengzeng <yaozengzeng@huawei.com>
2025-08-15 09:15:12 +0000

    fix cargo fmt
    Signed-off-by: YaoZengzeng <yaozengzeng@huawei.com>

--- orion-xds/examples/server.rs
+++ orion-xds/examples/server.rs
@@ -8,10 +8,7 @@ use tracing_subscriber::EnvFilter;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt()
-        .with_env_filter(
-            EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "info,orion_xds=debug".into()),
-        )
+        .with_env_filter(EnvFilter::try_from_default_env().unwrap_or_else(|_| "info,orion_xds=debug".into()))
         .init();
 
     let (delta_resource_tx, delta_resources_rx) = tokio::sync::mpsc::channel(100);
