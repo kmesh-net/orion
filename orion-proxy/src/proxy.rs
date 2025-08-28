522c9980a0b77e088f527ae83d7c7dba68a4b1f4
Nicola Bonelli <nicola.bonelli@huawei-partners.com>
2025-08-28 07:47:03 +0200

    Orion proxy: internal sync
    Refactor proxy runtime to support metrics and access logs
    
    Signed-off-by: Nicola Bonelli <nicola.bonelli@huawei-partners.com>
    
    Co-authored-by: Francesco Ciaccia <francesco.ciaccia1@huawei-partners.com>
    Co-authored-by: Dawid Nowak <nowakd@gmail.com>

--- orion-proxy/src/proxy.rs
+++ orion-proxy/src/proxy.rs
@@ -19,27 +19,39 @@
 //
 
 use crate::{
+    admin::start_admin_server,
     core_affinity,
     runtime::{self, RuntimeId},
     xds_configurator::XdsConfigurationHandler,
 };
+use compact_str::ToCompactString;
 use futures::future::join_all;
-use orion_configuration::config::{bootstrap::Node, runtime::Affinity, Bootstrap};
+use orion_configuration::config::{bootstrap::Node, log::AccessLogConfig, runtime::Affinity, Bootstrap};
 use orion_error::Context;
 use orion_lib::{
+    access_log::{start_access_loggers, update_configuration, Target},
     get_listeners_and_clusters, new_configuration_channel, runtime_config, ConfigurationReceivers,
     ConfigurationSenders, ListenerConfigurationChange, Result, SecretManager,
 };
-use std::thread::{self, JoinHandle};
+use orion_metrics::{
+    launch_metrics_exporter, metrics::init_global_metrics, wait_for_metrics_setup, Metrics, VecMetrics,
+};
+use parking_lot::RwLock;
+use std::{
+    sync::Arc,
+    thread::{self, JoinHandle},
+};
 use tokio::{sync::mpsc::Sender, task::JoinSet};
 use tracing::{debug, info, warn};
 
-pub fn run_proxy(bootstrap: Bootstrap) -> Result<()> {
+pub fn run_orion(bootstrap: Bootstrap, access_log_config: Option<AccessLogConfig>) -> Result<()> {
     debug!("Starting on thread {:?}", std::thread::current().name());
-    launch_runtimes(bootstrap).context("failed to launch runtimes")
+
+    // launch the runtimes...
+    launch_runtimes(bootstrap, access_log_config).with_context_msg("failed to launch runtimes")
 }
 
-fn calculate_threads_per_runtime(num_cpus: usize, num_runtimes: usize) -> Result<usize> {
+fn calculate_num_threads_per_runtime(num_cpus: usize, num_runtimes: usize) -> Result<usize> {
     let avail_cpus = core_affinity::get_avail_core_num()?;
     if num_cpus > avail_cpus {
         return Err(
@@ -64,40 +76,55 @@ fn calculate_threads_per_runtime(num_cpus: usize, num_runtimes: usize) -> Result
     Ok(threads)
 }
 
+#[derive(Debug, Clone)]
 struct ServiceInfo {
     bootstrap: Bootstrap,
     node: Node,
     configuration_senders: Vec<ConfigurationSenders>,
-    secret_manager: SecretManager,
+    secret_manager: Arc<RwLock<SecretManager>>,
     listener_factories: Vec<orion_lib::ListenerFactory>,
     clusters: Vec<orion_lib::PartialClusterType>,
     ads_cluster_names: Vec<String>,
+    access_log_config: Option<AccessLogConfig>,
 }
 
-fn launch_runtimes(bootstrap: Bootstrap) -> Result<()> {
+fn launch_runtimes(bootstrap: Bootstrap, access_log_config: Option<AccessLogConfig>) -> Result<()> {
     let rt_config = runtime_config();
-
     let num_runtimes = rt_config.num_runtimes();
     let num_cpus = rt_config.num_cpus();
+
+    // launch metrics exporters...
+    //
+
+    let metrics = VecMetrics::from(&bootstrap).0;
+    let metrics_handle = spawn_metrics_runtime_from_thread("metrics", 1, None, metrics.clone())?;
+
+    info!("Waiting for metrics setup to complete...");
+    wait_for_metrics_setup();
+
+    let num_threads_per_runtime = calculate_num_threads_per_runtime(num_cpus, num_runtimes)
+        .with_context_msg("failed to calculate number of threads to use per runtime")?;
+    info!("using {} runtimes with {num_threads_per_runtime} threads each", rt_config.num_runtimes());
+
+    // initialize global metrics...
+    init_global_metrics(&metrics, num_threads_per_runtime * num_runtimes);
+
     info!("Launching with {} cpus, {} runtimes", num_cpus, num_runtimes);
 
     let handles = {
-        let num_threads_per_runtime = calculate_threads_per_runtime(num_cpus, num_runtimes)
-            .context("failed to calculate number of threads to use per runtime")?;
-        info!("using {} runtimes with {num_threads_per_runtime} threads each", rt_config.num_runtimes());
-
         (0..num_runtimes)
             .map(|id| {
                 spawn_proxy_runtime_from_thread(
                     "proxy",
                     num_threads_per_runtime,
+                    metrics.clone(),
                     rt_config.affinity_strategy.clone().map(|affinity| (RuntimeId(id), affinity)),
                 )
             })
             .collect::<Result<Vec<_>>>()?
     };
 
-    let (handles, configuration_senders): (Vec<_>, Vec<_>) = handles.into_iter().unzip();
+    let (proxy_handles, configuration_senders): (Vec<_>, Vec<_>) = handles.into_iter().unzip();
 
     // The xDS runtime always runs - this is necessary for initialization even if we do not
     // use dynamic updates from remote xDS servers. The decision on whether dynamic updates
@@ -110,10 +137,11 @@ fn launch_runtimes(bootstrap: Bootstrap) -> Result<()> {
     // static clusters.
 
     let ads_cluster_names: Vec<String> = bootstrap.get_ads_configs().iter().map(ToString::to_string).collect();
-    let node = bootstrap.node.clone().unwrap_or_else(|| Node { id: "".into() });
+    let node = bootstrap.node.clone().unwrap_or_else(|| Node { id: "".into(), cluster_id: "".into() });
 
     let (secret_manager, listener_factories, clusters) =
-        get_listeners_and_clusters(bootstrap.clone()).context("Failed to get listeners and clusters")?;
+        get_listeners_and_clusters(bootstrap.clone()).with_context_msg("Failed to get listeners and clusters")?;
+    let secret_manager = Arc::new(RwLock::new(secret_manager));
 
     if listener_factories.is_empty() && ads_cluster_names.is_empty() {
         return Err("No listeners and no ads clusters configured".into());
@@ -127,12 +155,24 @@ fn launch_runtimes(bootstrap: Bootstrap) -> Result<()> {
         bootstrap,
         clusters,
         ads_cluster_names,
+        access_log_config,
     };
 
-    spawn_service_runtime_from_thread("services", rt_config.num_service_threads.get() as usize, None, service_info)?;
+    let services_handle = spawn_services_runtime_from_thread(
+        "services",
+        rt_config.num_service_threads.get() as usize,
+        None,
+        service_info,
+    )?;
 
-    for handle in handles {
-        if let Err(err) = handle.join() {
+    let handles = proxy_handles
+        .into_iter()
+        .chain(std::iter::once(metrics_handle))
+        .chain(std::iter::once(services_handle))
+        .collect::<Vec<_>>();
+
+    for h in handles {
+        if let Err(err) = h.join() {
             warn!("Closing handler with error {err:?}");
         }
     }
@@ -144,17 +184,15 @@ type RuntimeHandle = JoinHandle<Result<()>>;
 fn spawn_proxy_runtime_from_thread(
     thread_name: &'static str,
     num_threads: usize,
+    metrics: Vec<Metrics>,
     affinity_info: Option<(RuntimeId, Affinity)>,
 ) -> Result<(RuntimeHandle, ConfigurationSenders)> {
     let (configuration_senders, configuration_receivers) = new_configuration_channel(100);
 
-    let thread_name = match &affinity_info {
-        Some((runtime_id, _affinity)) => format!("{thread_name}_RT{runtime_id}"),
-        None => format!("{thread_name}_rt"),
-    };
+    let thread_name = build_thread_name(thread_name, affinity_info.as_ref());
 
     let handle: JoinHandle<Result<()>> = thread::Builder::new().name(thread_name.clone()).spawn(move || {
-        let rt = runtime::build_tokio_runtime(&thread_name, num_threads, affinity_info);
+        let rt = runtime::build_tokio_runtime(&thread_name, num_threads, affinity_info, Some(metrics));
         rt.block_on(async {
             tokio::select! {
                 _ = start_proxy(configuration_receivers) => {
@@ -171,22 +209,38 @@ fn spawn_proxy_runtime_from_thread(
     Ok((handle, configuration_senders))
 }
 
-fn spawn_service_runtime_from_thread(
+fn spawn_metrics_runtime_from_thread(
+    thread_name: &'static str,
+    num_threads: usize,
+    affinity_info: Option<(RuntimeId, Affinity)>,
+    metrics: Vec<Metrics>,
+) -> Result<RuntimeHandle> {
+    let thread_name = build_thread_name(thread_name, affinity_info.as_ref());
+    let rt_handle = thread::Builder::new().name(thread_name.clone()).spawn(move || {
+        let rt = runtime::build_tokio_runtime(&thread_name, num_threads, affinity_info, None);
+        rt.block_on(async {
+            launch_metrics_exporter(&metrics).await?;
+            tokio::signal::ctrl_c().await?;
+            info!("CTRL+C received. Shutting down metrics runtime.");
+            Ok(())
+        })
+    })?;
+    Ok(rt_handle)
+}
+
+fn spawn_services_runtime_from_thread(
     thread_name: &'static str,
     num_threads: usize,
     affinity_info: Option<(RuntimeId, Affinity)>,
     service_info: ServiceInfo,
 ) -> Result<RuntimeHandle> {
-    let thread_name = match &affinity_info {
-        Some((runtime_id, _affinity)) => format!("{thread_name}_RT{runtime_id}"),
-        None => format!("{thread_name}_rt"),
-    };
+    let thread_name = build_thread_name(thread_name, affinity_info.as_ref());
 
     let rt_handle = thread::Builder::new().name(thread_name.clone()).spawn(move || {
-        let rt = runtime::build_tokio_runtime(&thread_name, num_threads, affinity_info);
+        let rt = runtime::build_tokio_runtime(&thread_name, num_threads, affinity_info, None);
         rt.block_on(async {
             tokio::select! {
-                () = run_services(service_info) => {
+                () = spawn_services(service_info) => {
                     info!("Service Runtime terminated!");
                     Ok(())
                 }
@@ -201,37 +255,82 @@ fn spawn_service_runtime_from_thread(
     Ok(rt_handle)
 }
 
-#[allow(clippy::too_many_arguments)]
-async fn run_services(info: ServiceInfo) {
+fn build_thread_name(thread_name: &'static str, affinity_info: Option<&(RuntimeId, Affinity)>) -> String {
+    match affinity_info {
+        Some((runtime_id, _)) => format!("{thread_name}_RT{runtime_id}"),
+        None => format!("{thread_name}_RT"),
+    }
+}
+
+async fn spawn_services(info: ServiceInfo) {
     let ServiceInfo {
-        bootstrap: _,
+        bootstrap,
         node,
         configuration_senders,
         secret_manager,
         listener_factories,
         clusters,
         ads_cluster_names,
+        access_log_config,
     } = info;
     let mut set: JoinSet<Result<()>> = JoinSet::new();
 
     // spawn XSD configuration service...
-
+    let configuration_senders_clone = configuration_senders.clone();
+    let bootstrap_clone = bootstrap.clone();
+    let secret_manager_clone = secret_manager.clone();
     set.spawn(async move {
-        let secret_manager =
-            configure_initial_resources(secret_manager, listener_factories, configuration_senders.clone()).await?;
-        let xds_handler = XdsConfigurationHandler::new(secret_manager, configuration_senders);
+        configure_initial_resources(bootstrap_clone, listener_factories, configuration_senders_clone.clone()).await?;
+        let xds_handler = XdsConfigurationHandler::new(secret_manager_clone, configuration_senders_clone);
         _ = xds_handler.xds_run(node, clusters, ads_cluster_names).await;
         Ok(())
     });
+
+    // spawn access loggers service...
+    if let Some(conf) = access_log_config {
+        let listeners = bootstrap.static_resources.listeners.clone();
+        set.spawn(async move {
+            let handles = start_access_loggers(
+                conf.num_instances.get(),
+                conf.queue_length.get(),
+                conf.log_rotation.0.clone(),
+                conf.max_log_files.get(),
+            );
+
+            info!("Access loggers started with {} instances", conf.num_instances);
+
+            let listener_configurations =
+                listeners.iter().map(|l| (l.name.clone(), l.get_access_log_configurations())).collect::<Vec<_>>();
+
+            for (listener_name, access_log_configurations) in listener_configurations {
+                _ = update_configuration(
+                    Target::Listener(listener_name.to_compact_string()),
+                    access_log_configurations,
+                )
+                .await;
+            }
+
+            handles.join_all().await;
+            Ok(())
+        });
+    }
+
+    // spawn admin interface task
+    if bootstrap.admin.is_some() {
+        set.spawn(async move {
+            _ = start_admin_server(bootstrap, configuration_senders, secret_manager).await;
+            Ok(())
+        });
+    }
 
     set.join_all().await;
 }
 
 async fn configure_initial_resources(
-    secret_manager: SecretManager,
+    bootstrap: Bootstrap,
     listeners: Vec<orion_lib::ListenerFactory>,
     configuration_senders: Vec<ConfigurationSenders>,
-) -> Result<SecretManager> {
+) -> Result<()> {
     let listeners_tx: Vec<_> = configuration_senders
         .into_iter()
         .map(|ConfigurationSenders { listener_configuration_sender, route_configuration_sender: _ }| {
@@ -239,14 +338,17 @@ async fn configure_initial_resources(
         })
         .collect();
 
-    for listener in listeners {
+    for (listener, listener_conf) in listeners.iter().zip(bootstrap.static_resources.listeners) {
         let _ = join_all(listeners_tx.iter().map(|listener_tx: &Sender<ListenerConfigurationChange>| {
-            listener_tx.send(ListenerConfigurationChange::Added(listener.clone()))
+            listener_tx.send(ListenerConfigurationChange::Added(Box::new((listener.clone(), listener_conf.clone()))))
         }))
-        .await;
+        .await
+        .into_iter()
+        .collect::<std::result::Result<Vec<_>, _>>()
+        .map_err(Into::<orion_error::Error>::into)?;
     }
 
-    Ok(secret_manager)
+    Ok(())
 }
 
 async fn start_proxy(configuration_receivers: ConfigurationReceivers) -> Result<()> {
