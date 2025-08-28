115e10351b5905f0c3da6dd4f5c871963381c891
Nicola Bonelli <nicola.bonelli@huawei-partners.com>
2025-08-28 07:02:43 +0200

    Move header documentation into macro invocation
    Signed-off-by: Nicola Bonelli <nicola.bonelli@huawei-partners.com>

--- orion-http-header/src/lib.rs
+++ orion-http-header/src/lib.rs
@@ -27,53 +27,70 @@ macro_rules! custom_header {
     };
 }
 
-/// The `x-envoy-original-path` header is used to store the original request path
-custom_header!(X_ENVOY_ORIGINAL_PATH, "x-envoy-original-path");
+custom_header!(
+    /// The `x-envoy-original-path` header is used to store the original request path
+    X_ENVOY_ORIGINAL_PATH, "x-envoy-original-path");
 
-/// The `B3` header is used for B3 single header tracing
-custom_header!(B3, "b3");
+custom_header!(
+    /// The `B3` header is used for B3 single header tracing
+    B3, "b3");
 
-/// The `uber-trace-id` header is used for Jaeger tracing
-custom_header!(UBER_TRACE_ID, "uber-trace-id");
+custom_header!(
+    /// The `uber-trace-id` header is used for Jaeger tracing
+    UBER_TRACE_ID, "uber-trace-id");
 
-/// The `x-b3-traceid` header is used for B3 tracing
-custom_header!(X_B3_TRACEID, "x-b3-traceid");
+custom_header!(
+    /// The `x-b3-traceid` header is used for B3 tracing
+    X_B3_TRACEID, "x-b3-traceid");
 
-/// The `x-b3-spanid` header is used for B3 tracing
-custom_header!(X_B3_SPANID, "x-b3-spanid");
+custom_header!(
+    /// The `x-b3-spanid` header is used for B3 tracing
+    X_B3_SPANID, "x-b3-spanid");
 
-/// The `x-b3-parentspanid` header is used for B3 tracing
-custom_header!(X_B3_PARENTSPANID, "x-b3-parentspanid");
+custom_header!(
+    /// The `x-b3-parentspanid` header is used for B3 tracing
+    X_B3_PARENTSPANID, "x-b3-parentspanid");
 
-/// The `x-b3-sampled` header is used for B3 tracing
-custom_header!(X_B3_SAMPLED, "x-b3-sampled");
+custom_header!(
+    /// The `x-b3-sampled` header is used for B3 tracing
+    X_B3_SAMPLED, "x-b3-sampled");
 
-/// The `X-Envoy-Force-Trace` header is used to force tracing in Envoy
-custom_header!(X_ENVOY_FORCE_TRACE, "x-envoy-force-trace");
+custom_header!(
+    /// The `X-Envoy-Force-Trace` header is used to force tracing in Envoy
+    X_ENVOY_FORCE_TRACE, "x-envoy-force-trace");
 
-/// The `x-datadog-trace-id` header is used for Datadog tracing
-custom_header!(X_DATADOG_TRACE_ID, "x-datadog-trace-id");
+custom_header!(
+    /// The `x-datadog-trace-id` header is used for Datadog tracing
+    X_DATADOG_TRACE_ID, "x-datadog-trace-id");
 
-/// The `X-Request-ID` header is used to pass a unique request ID
-custom_header!(X_REQUEST_ID, "x-request-id");
+custom_header!(
+    /// The `X-Request-ID` header is used to pass a unique request ID
+    X_REQUEST_ID, "x-request-id");
 
-/// The `x-client-trace-id` header is used to pass a client trace ID
-custom_header!(X_CLIENT_TRACE_ID, "x-client-trace-id");
+custom_header!(
+    /// The `x-client-trace-id` header is used to pass a client trace ID
+    X_CLIENT_TRACE_ID, "x-client-trace-id");
 
-/// The `traceparent` header is used for W3C Trace Context
-custom_header!(TRACEPARENT, "traceparent");
+custom_header!(
+    /// The `traceparent` header is used for W3C Trace Context
+    TRACEPARENT, "traceparent");
 
-/// The `X-Envoy-RateLimited` header is used to indicate rate limiting by Envoy
-custom_header!(X_ENVOY_RATELIMITED, "x-envoy-ratelimited");
+custom_header!(
+    /// The `X-Envoy-RateLimited` header is used to indicate rate limiting by Envoy
+    X_ENVOY_RATELIMITED, "x-envoy-ratelimited");
 
-/// The `x-orion-ratelimited` header is used to indicate rate limiting by Orion
-custom_header!(X_ORION_RATELIMITED, "x-orion-ratelimited");
+custom_header!(
+    /// The `x-orion-ratelimited` header is used to indicate rate limiting by Orion
+    X_ORION_RATELIMITED, "x-orion-ratelimited");
 
-/// The `x-forwarded-for` header is used to identify the originating IP address of a client
-custom_header!(X_FORWARDED_FOR, "x-forwarded-for");
+custom_header!(
+    /// The `x-forwarded-for` header is used to identify the originating IP address of a client
+    X_FORWARDED_FOR, "x-forwarded-for");
 
-/// The `x-envoy-external-address` header is used to pass the external address in Envoy
-custom_header!(X_ENVOY_EXTERNAL_ADDRESS, "x-envoy-external-address");
+custom_header!(
+    /// The `x-envoy-external-address` header is used to pass the external address in Envoy
+    X_ENVOY_EXTERNAL_ADDRESS, "x-envoy-external-address");
 
-/// The `x-envoy-internal` header is used to indicate internal requests in Envoy
-custom_header!(X_ENVOY_INTERNAL, "x-envoy-internal");
+custom_header!(
+    /// The `x-envoy-internal` header is used to indicate internal requests in Envoy
+    X_ENVOY_INTERNAL, "x-envoy-internal");
