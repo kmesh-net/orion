e60ef1ad2ceed9720a5519b44c67c88982e0223c
Eeshu-Yadav <eeshuyadav123@gmail.com>
2025-09-12 04:46:18 +0530

    Add CI job to check copyright headers in Rust files
    Signed-off-by: Eeshu-Yadav <eeshuyadav123@gmail.com>

--- orion-data-plane-api/tests/bootstrap.rs
+++ orion-data-plane-api/tests/bootstrap.rs
@@ -1,6 +1,5 @@
 // Copyright 2025 The kmesh Authors
 //
-//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -13,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-
 use envoy_data_plane_api::envoy::config::{
     bootstrap::v3::Bootstrap,
     core::v3::{address, socket_address::PortSpecifier, Address, SocketAddress},
